891d97a4ef8016a4a573c77344c116788f5b02f8
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-06-29 19:36:40 +0530

    React App

--- shopping_cart/src/App.js
+++ shopping_cart/src/App.js
@@ -1,13 +1,41 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
+import React, { useState } from 'react';
+import Navbar from './Components/Navbar';
 import Shop from './Components/Shop';
+import './Styles/App.css';
+
 const App = () => {
+  const [cart, setCart] = useState([]);
+  const [warning, setWarning] = useState(false);
+
+  const handleClick = (item) => {
+    let isPresent = false;
+    cart.forEach((product) => {
+      if (item.id === product.id) {
+        isPresent = true;
+      }
+    });
+    if (isPresent) {
+      setWarning(true);
+      setTimeout(()=>{
+        setWarning(false)},
+        2000
+      );
+      return
+    } 
+    else {
+      setCart([...cart, item]);
+    }
+  };
+
   return (
     <div>
-      <Navbar/>
-      <Shop/>
+      <Navbar size={cart.length} />
+      <Shop handleClick={handleClick} />
+      {warning && <div className='warning'>
+        Item Already Present in Cart
+        </div>}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
