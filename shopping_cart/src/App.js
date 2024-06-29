a2727f1e75d3948d5a2e68318b8b05b4a8e7e38d
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-06-29 19:35:47 +0530

    CSS Styles

---
+++ shopping_cart/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Shop from './Components/Shop';
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Shop/>
+    </div>
+  )
+}
+
+export default App
