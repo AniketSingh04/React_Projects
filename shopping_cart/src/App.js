72970a5d7248335134caebf85c246037923d7d82
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-06-29 20:25:57 +0530

    App

--- shopping_cart/src/App.js
+++ shopping_cart/src/App.js
@@ -2,10 +2,12 @@ import React, { useState } from 'react';
 import Navbar from './Components/Navbar';
 import Shop from './Components/Shop';
 import './Styles/App.css';
+import Cart from './Components/Cart';
 
 const App = () => {
   const [cart, setCart] = useState([]);
   const [warning, setWarning] = useState(false);
+  const [showShop, setShowShop] = useState(true); // State to control which component to show
 
   const handleClick = (item) => {
     let isPresent = false;
@@ -16,24 +18,28 @@ const App = () => {
     });
     if (isPresent) {
       setWarning(true);
-      setTimeout(()=>{
-        setWarning(false)},
-        2000
-      );
-      return
-    } 
-    else {
+      setTimeout(() => {
+        setWarning(false);
+      }, 2000);
+      return;
+    } else {
       setCart([...cart, item]);
     }
   };
 
   return (
     <div>
-      <Navbar size={cart.length} />
-      <Shop handleClick={handleClick} />
-      {warning && <div className='warning'>
-        Item Already Present in Cart
-        </div>}
+      <Navbar size={cart.length} setShowShop={setShowShop}/>
+      {showShop ? (
+        <Shop handleClick={handleClick} />
+      ) : (
+        <Cart cart={cart} setCart={setCart} warning={warning} />
+      )}
+      {warning && (
+        <div className='warning'>
+          Item Already Present in Cart
+        </div>
+      )}
     </div>
   );
 };
